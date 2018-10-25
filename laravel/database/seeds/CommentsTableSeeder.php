86e06e36aca089cb39275f126cdeb9a0b2c1ffe8
Tobias Marschallek <tobias.marschallek@wtl.de>
2018-10-25 15:32:56 +0200

    commentseeder

--- laravel/database/seeds/CommentsTableSeeder.php
+++ laravel/database/seeds/CommentsTableSeeder.php
@@ -11,6 +11,13 @@ class CommentsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        for ($i = 0; $i < 100; $i++) {
+            DB::table('comments')->insert([
+                'post_id' => rand(1,100),
+                'message' => str_random(150),
+                'created_at' => now(),
+                'updated_at' => now()
+            ]);
+        }
     }
 }
