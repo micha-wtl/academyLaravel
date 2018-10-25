ca3708acf67522e83a728201196b8c1116214518
Tobias Marschallek <tobias.marschallek@wtl.de>
2018-10-25 15:26:01 +0200

    posts hinzugefügt

--- laravel/database/seeds/PostsTableSeeder.php
+++ laravel/database/seeds/PostsTableSeeder.php
@@ -11,6 +11,13 @@ class PostsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        for ($i = 0; $i < 100; $i++) {
+            DB::table('posts')->insert([
+                'user_id' => rand(1,100),
+                'message' => str_random(100),
+                'created_at' => now(),
+                'updated_at' => now()
+            ]);
+        }
     }
 }
--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
-use App\User;
+use App\Http\Resources\PostsCollection;
 use App\Http\Resources\UserCollection;
+use App\Post;
+use App\User;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,9 +17,6 @@ use App\Http\Resources\UserCollection;
 |
 */
 
-//Route::middleware('auth:api')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
 
 Route::group(['middleware' => 'auth:api'], function () {
 
@@ -67,6 +66,150 @@ Route::group(['middleware' => 'auth:api'], function () {
         $user->delete();
 
         return response('{}', 200);
+    });
+
+
+    /*
+     * List all posts
+     *
+     *
+     *
+        {
+          "posts": [
+            {
+              "id": 1,
+              "message": "Das ist ein Test!",
+              "author": {
+                "id": 1,
+                "name": "Uwe Kowalsky"
+              },
+              "comments": []
+            },
+            {
+              "id": 2,
+              "message": "Das ist ein zweiter Test!",
+              "author": {
+                "id": 1,
+                "name": "Uwe Kowalsky"
+              },
+              "comments": []
+            }
+          ]
+        }
+     *
+     *
+     */
+    Route::get('/posts', function () {
+
+        $a = new PostsCollection(Post::all('id', 'user_id', 'message'));
+        $ret = array();
+
+        foreach ($a as $b) {
+            $user_id = Post::findOrFail($b['id'], array('user_id'));
+            $author = User::find($user_id);
+
+            $js = array(
+                "id" => $b['id'],
+                "message" =>  $b['message'],
+                "author" => array(
+                    "id" => $b['user_id'],
+                    "name" => $author->first()->name
+                ),
+                "comments" => ""
+            );
+
+            $ret[] = $js;
+        }
+
+
+       return json_encode(array("posts" =>$ret));
+
+
+    });
+
+    /*
+     * delete post
+     */
+    Route::delete('/posts/{id}', function ($id) {
+        $posts = App\Post::find($id);
+
+        $posts->delete();
+
+        return response('ok', 200)->header('Content-Type', 'text/plain');
+    });
+
+
+    /*
+     * get single post
+     *
+            {
+              "id": 1,
+              "message": "Das ist ein Test!",
+              "author": {
+                "id": 1,
+                "name": "Uwe Kowalsky"
+              },
+              "comments": []
+            }
+     */
+    Route::get('/posts/{id}', function ($id) {
+
+        $message = Post::findOrFail($id, array('message'));
+        $user_id = Post::findOrFail($id, array('user_id'));
+        $author = User::find($user_id);
+
+
+        $a = array(
+            "id" => Auth::id(),
+            "message" => $message,
+            "author" => array(
+                "id" => $user_id,
+                "name" => $author->first()->name
+            ),
+            "comments" => ""
+
+        );
+
+        return json_encode($a);
+
+    });
+
+
+    /*
+     * add new post
+     *
+     *
+            {
+              "id": 1,
+              "message": "Das ist ein Test!",
+              "author": {
+                "id": 1,
+                "name": "Uwe Kowalsky"
+              }
+            }
+     */
+    Route::post('/posts', function (Request $request) {
+
+        $post = new Post();
+        $post->message = $request->message;
+        $post->created_at = now();
+        $post->user_id = Auth::id();
+        $post->save();
+
+        /*
+         * user raussuchen
+         */
+
+        $a = array(
+            "id" => Auth::id(),
+            "message" => $request->message,
+            "author" => array(
+                "id" => Auth::id(),
+                "name" => Auth::user()->name,
+            )
+        );
+
+        return json_encode($a);
     });
 
 
